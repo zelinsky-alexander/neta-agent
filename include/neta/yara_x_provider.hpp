225f4cb3ddffce21a3988a110f48085cbe51b09b
zelinsky alexander <34163821+zelinsky-alexander@users.noreply.github.com>
2026-09-03 09:26:46 +0300

    Add YARA-X provider interface

---
+++ include/neta/yara_x_provider.hpp
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "neta/antimalware.hpp"
+
+#include <cstddef>
+#include <filesystem>
+#include <memory>
+#include <string>
+
+namespace neta {
+
+struct YaraXProviderConfig {
+    std::filesystem::path rules_path;
+    std::string ruleset_id{"default"};
+    std::size_t max_artifact_bytes{64U * 1024U * 1024U};
+    std::uint64_t timeout_seconds{5};
+};
+
+class YaraXProvider final : public AntimalwareProvider {
+public:
+    explicit YaraXProvider(YaraXProviderConfig config);
+    ~YaraXProvider() override;
+
+    YaraXProvider(const YaraXProvider&) = delete;
+    YaraXProvider& operator=(const YaraXProvider&) = delete;
+    YaraXProvider(YaraXProvider&&) noexcept;
+    YaraXProvider& operator=(YaraXProvider&&) noexcept;
+
+    [[nodiscard]] std::string name() const override;
+    [[nodiscard]] AntimalwareEvidence scan(const ArtifactIdentity& artifact) override;
+    [[nodiscard]] bool available() const noexcept;
+
+private:
+    class Impl;
+    std::unique_ptr<Impl> impl_;
+};
+
+}  // namespace neta
